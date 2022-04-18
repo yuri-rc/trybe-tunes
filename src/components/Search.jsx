939861d45a2a3c661ad5295d71773a432bc97736
Yuri-RC <cttyuriresende@gmail.com>
2022-04-18 19:38:43 -0300

    requisito 5

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,11 +3,35 @@ import Header from './Header';
 // import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      disable: true,
+    };
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange = ({ target: { value } }) => {
+    this.setState({ disable: value.length < 2 });
+  };
+
   render() {
+    const { disable } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <h1>Search</h1>
+        <input
+          data-testid="search-artist-input"
+          type="text"
+          onChange={ this.onChange }
+        />
+        <button
+          data-testid="search-artist-button"
+          type="button"
+          disabled={ disable }
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
